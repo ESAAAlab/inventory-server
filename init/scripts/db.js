2a5949c0a99221ab190de4510fe37ad314431367
Kamel Makhloufi <melka@blaste.net>
2017-09-13 10:17:22 +0200

    DB init

---
+++ init/scripts/db.js
@@ -0,0 +1,235 @@
+var models = require('../models/index')
+var fixtures = require('sequelize-fixtures')
+var request = require('request')
+var throttledRequest = require('throttled-request')(request)
+var md5 = require('md5')
+var jsonQuery = require('json-query')
+var Mockaroo = require('mockaroo')
+
+throttledRequest.configure({
+  requests: 5,
+  milliseconds: 2000
+})
+
+module.exports = {
+
+  ers: 200,
+  nbInventory: 1000,
+
+  drop: function () {
+    console.log('dropping tables')
+    models.sequelize.drop()
+  },
+
+  sync: function () {
+    console.log('synching tables')
+    models.sequelize.sync({
+      force: true
+    }).then(
+      function () {
+        fixtures.loadFile('scripts/assets/init.json', models).then(function () {})
+      }
+    )
+  },
+
+  randomInventory: function (nb) {
+    console.log('Generating ' + nb + ' random inventory items')
+
+    var client = new Mockaroo.Client({
+      apiKey: '173c4280' // see http://mockaroo.com/api/docs to get your api key
+    })
+
+    client.generate({
+      count: nb,
+      fields: [{
+        name: 'brand',
+        type: 'Company Name'
+      }, {
+        name: 'model',
+        type: 'App Name'
+      }, {
+        name: 'name',
+        type: 'Formula',
+        value: 'brand+" "+model'
+      }, {
+        name: 'serialNumber',
+        type: 'ISBN'
+      }, {
+        name: 'barcode',
+        type: 'Formula',
+        value: 'digest(name+" "+serialNumber,"MD5")'
+      }, {
+        name: 'materialCode',
+        type: 'Credit Card #'
+      }, {
+        name: 'inventoryNumber',
+        type: 'Credit Card #'
+      }, {
+        name: 'acquisitionPrice',
+        type: 'Number',
+        min: 0,
+        max: 5000,
+        decimals: 2
+      }, {
+        name: 'acquisitionDate',
+        type: 'Date',
+        min: '1/1/2000',
+        max: '08/31/2016',
+        format: '%m/%d/%Y'
+      }, {
+        name: 'description',
+        type: 'Paragraphs',
+        max: 1
+      }, {
+        name: 'isConsummable',
+        type: 'Boolean'
+      }, {
+        name: 'stockMax',
+        type: 'Number',
+        min: 1,
+        max: 100,
+        decimals: 0
+      }, {
+        name: 'stockAvailable',
+        type: 'Formula',
+        value: 'random(0,stockMax)'
+      }, {
+        name: 'stockStep',
+        type: 'Formula',
+        value: 'random(1,stockMax)'
+      }, {
+        name: 'stockUnit',
+        type: 'Custom List',
+        values: ['pc', 'g', 'Kg', 'L']
+      },
+      {
+        name: 'itemCategoryId',
+        type: 'Number',
+        min: 8,
+        max: 40,
+        decimals: 0
+      },
+      {
+        name: 'itemLocationId',
+        type: 'Formula',
+        value: 'if itemCategoryId <= 20 then 1 else 2 end'
+      }
+      ]
+    }).then(function (records) {
+      console.log(records)
+      for (var i in records) {
+        var item = records[i]
+        models.item.create({
+          name: item.name,
+          model: item.model,
+          brand: item.brand,
+          serialNumber: item.serialNumber,
+          materialCode: item.materialCode,
+          barcode: item.barcode,
+          inventoryNumber: item.inventoryNumber,
+          acquisitionPrice: item.acquisitionPrice,
+          acquisitionDate: item.acquisitionDate,
+          description: item.description,
+          isConsummable: item.isConsummable,
+          stockStep: item.stockStep,
+          stockUnit: item.stockUnit,
+          stockMax: item.stockMax,
+          stockAvailable: item.stockAvailable,
+          itemCategoryId: item.itemCategoryId,
+          itemLocationId: item.itemLocationId
+        }).then(function (sqlitem) {
+          /* var qres = jsonQuery('[barcode=' + sqlitem.barcode + ']', {
+            data: records
+          })
+          models.transaction.create({
+            type: 'stockCount',
+            startDate: new Date(),
+            ended: true,
+            quantity: records[qres.key].stockMax
+          }).then(function(sqltransaction) {
+            sqlitem.addStockCount(sqltransaction);
+          }) */
+        })
+      }
+    }).catch(function (error) {
+      if (error instanceof Mockaroo.errors.InvalidApiKeyError) {
+        console.log('invalid api key')
+      } else if (error instanceof Mockaroo.errors.UsageLimitExceededError) {
+        console.log('usage limit exceeded')
+      } else if (error instanceof Mockaroo.errors.ApiError) {
+        console.log('api error: ' + error.message)
+      } else {
+        console.log('unknown error: ' + error)
+      }
+    })
+  },
+
+  randomUsers: function (nb) {
+    console.log('querying randomuser.me for ' + nb + ' users')
+    var randomUri = 'http://api.randomuser.me/?inc=name,email,phone,cell,location,picture,login&nat=fr&noinfo&results=' + nb
+    var maxStudentYear = 11
+    var maxUserType = 7
+    var currentIndex = 0
+    request(randomUri, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var data = JSON.parse(body)
+        for (var i in data.results) {
+          var user = data.results[i]
+          console.log('creating user ' + (i * 1 + 1) + ' : ' + user.name.first + ' ' + user.name.last)
+          models.user.create({
+            barcode: md5(user.name.first + user.name.last + user.login.salt),
+            title: user.name.title,
+            firstName: user.name.first,
+            lastName: user.name.last,
+            isRegistered: Math.random() < 0.5,
+            email: user.email,
+            cellPhone: user.cell,
+            homePhone: user.phone,
+            addressStreet: user.location.street,
+            addressCity: user.location.city,
+            addressState: user.location.state,
+            addressPostcode: user.location.postcode,
+            loginUsername: user.login.username,
+            loginPassword: user.login.password, // FOR DEVELOPMENT ONLY, MUST BE REMOVED IN PRODUCTION
+            loginSalt: user.login.salt,
+            loginMD5: user.login.md5,
+            loginSHA1: user.login.sha1,
+            loginSHA256: user.login.sha256,
+            userTypeId: Math.ceil(Math.random() * maxUserType),
+            studentYearId: Math.ceil(Math.random() * maxStudentYear)
+          }).then(function (sqluser) {
+            // find picture uri
+            var qres = jsonQuery('results.login[md5=' + sqluser.loginMD5 + ']', {
+              data: data
+            })
+            var uri = data.results[qres.key].picture.large
+            // download picture and create entry in database
+            throttledRequest({
+              uri: uri,
+              encoding: 'binary'
+            }, function (error, response, body) {
+              currentIndex++
+              var index = currentIndex
+              console.log('downloading thumbnail for user ' + index + '/' + nb + ' : ' + sqluser.firstName + ' ' + sqluser.lastName)
+              if (!error && response.statusCode === 200) {
+                var dataUriPrefix = 'data:' + response.headers['content-type'] + ';base64,'
+                var image = Buffer.from(body.toString(), 'binary').toString('base64')
+                image = dataUriPrefix + image
+                models.document.create({
+                  type: 'userProfilePic',
+                  content: image
+                }).then(function (pic) {
+                  sqluser.addPicture(pic)
+                })
+                console.log('userProfilePic added for user ' + index + '/' + nb + ' : ' + sqluser.firstName + ' ' + sqluser.lastName)
+              } else {
+                console.error('error downloading picture for user ' + index + '/' + nb + ' : ' + sqluser.firstName + ' ' + sqluser.lastName)
+                console.error(error)
+              }
+            })
+          })
+        }
+      }
+    })
+  }
+}
