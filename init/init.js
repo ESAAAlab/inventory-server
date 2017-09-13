8eb2d81524838d35741db0030c04418c5b799932
Kamel Makhloufi <melka@blaste.net>
2017-09-13 14:59:47 +0200

    Still needs a manual setup method for connection

--- init/init.js
+++ init/init.js
@@ -1,7 +1,5 @@
 var db = require('./scripts/db.js')
 
-students.loadMissingPicture()
-
 var myArgs = process.argv.slice(2)
 switch (myArgs[0]) {
   case 'drop':
@@ -19,3 +17,19 @@ switch (myArgs[0]) {
   default:
     console.log('Sorry, that is not something I know how to do.')
 }
+
+
+/*
+su - postgres
+psql -c "ALTER USER postgres PASSWORD 'dzer38'"";
+
+Then
+
+CREATE USER inventory WITH PASSWORD 'inventory';
+CREATE DATABASE inventory_dev;
+CREATE DATABASE inventory_test;
+CREATE DATABASE inventory_prod;
+GRANT ALL PRIVILEGES ON DATABASE inventory_dev to inventory;
+GRANT ALL PRIVILEGES ON DATABASE inventory_test to inventory;
+GRANT ALL PRIVILEGES ON DATABASE inventory_prod to inventory;
+ */
