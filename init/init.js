1de277221050f6f0d7aaa913fc06042a38fac95a
Kamel Makhloufi <melka@blaste.net>
2017-10-05 18:13:22 +0200

    removing hostname, changing with IP

--- init/init.js
+++ init/init.js
@@ -17,12 +17,3 @@ switch (myArgs[0]) {
   default:
     console.log('Sorry, that is not something I know how to do.')
 }
-
-/*
-su - postgres
-psql -c "ALTER USER postgres PASSWORD 'password';"
-psql -c "CREATE USER inventory WITH PASSWORD 'inventory';"
-psql -c "CREATE DATABASE inventory_dev;"
-psql -c "CREATE DATABASE inventory_prod;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE inventory_dev, inventory_prod to inventory;"
- */
