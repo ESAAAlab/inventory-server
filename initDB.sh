452c16808b7f5bf0e646803b951fdea3339326c7
melka <melka@blaste.net>
2017-10-07 13:04:54 +0200

    home dev

--- initDB.sh
+++ initDB.sh
@@ -6,9 +6,9 @@ su - postgres -c 'mkdir /data/pgsql/main'
 su - postgres -c '/usr/lib/postgresql/9.4/bin/initdb -D /data/pgsql/main'
 /etc/init.d/postgresql restart
 
-su - postgres
-psql -c "ALTER USER postgres PASSWORD 'password';"
-psql -c "CREATE USER inventory WITH PASSWORD 'inventory';"
-psql -c "CREATE DATABASE inventory_dev;"
-psql -c "CREATE DATABASE inventory_prod;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE inventory_dev, inventory_prod to inventory;"
+#su - postgres
+#psql -c "ALTER USER postgres PASSWORD 'password';"
+#psql -c "CREATE USER inventory WITH PASSWORD 'inventory';"
+#psql -c "CREATE DATABASE inventory_dev;"
+#psql -c "CREATE DATABASE inventory_prod;"
+#psql -c "GRANT ALL PRIVILEGES ON DATABASE inventory_dev, inventory_prod to inventory;"
