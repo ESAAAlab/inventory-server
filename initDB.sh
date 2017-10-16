84f9903816198ad8e000d41f35414223f52a8e5e
Kamel Makhloufi <melka@blaste.net>
2017-10-16 15:41:44 +0200

    Generating locale & database as fr_FR.UTF8 for unaccent

--- initDB.sh
+++ initDB.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# Generate fr_FR.UTF-8 Locale
+/etc/init.d/postgresql stop
+
+apt-get update
+apt-get install postgresql-contrib-9.4
+
+echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
 chown postgres:postgres /data/
 su - postgres -c 'mkdir /data/pgsql'
 su - postgres -c 'mkdir /data/pgsql/main'
-su - postgres -c '/usr/lib/postgresql/9.4/bin/initdb -D /data/pgsql/main'
-/etc/init.d/postgresql restart
+su - postgres -c '/usr/lib/postgresql/9.4/bin/initdb -D /data/pgsql/main --encoding=UTF8 --locale=fr_FR.UTF8'
+/etc/init.d/postgresql start
 
 #Change passwords and user for your configuration
 #su - postgres
